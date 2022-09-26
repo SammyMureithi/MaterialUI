5415c28f598eb6ab886c9edfe517bcbe204abbe3
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-26 12:43:43 +0300

    MUIFloatingActionButtons

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -2,13 +2,15 @@ import './App.css';
 import MaterialUI from './Components/MaterialUI';
 import MuiButton from './Components/MuiButton';
 import MUiCheckBox from './Components/MUiCheckBox';
+import MUIFloatingActionButton from './Components/MUIFloatingActionButton';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
       <MuiButton />
-      <MUiCheckBox/>
+      <MUiCheckBox />
+      <MUIFloatingActionButton/>
     </div>
   );
 }
