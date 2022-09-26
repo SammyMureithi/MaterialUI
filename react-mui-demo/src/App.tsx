e592c7ed9dfd084aeb75a4d034e3d8aa3f1b1024
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-26 12:53:03 +0300

    MUIRadioButtons

--- react-mui-demo/src/App.tsx
+++ react-mui-demo/src/App.tsx
@@ -3,6 +3,7 @@ import MaterialUI from './Components/MaterialUI';
 import MuiButton from './Components/MuiButton';
 import MUiCheckBox from './Components/MUiCheckBox';
 import MUIFloatingActionButton from './Components/MUIFloatingActionButton';
+import RadioButtons from './Components/RadioButtons';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <h1>Hello</h1>
       <MuiButton />
       <MUiCheckBox />
-      <MUIFloatingActionButton/>
+      <MUIFloatingActionButton />
+      <RadioButtons/>
     </div>
   );
 }
