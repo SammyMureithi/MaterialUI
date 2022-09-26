cf0cb7300ae35eafc220cd520376e5b7fc05c8a4
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-26 12:21:53 +0300

    MUIButtons

---
+++ react-mui-demo/src/Components/MuiButton.tsx
@@ -0,0 +1,12 @@
+import {Button} from '@mui/material'
+function MuiButton() {
+  return (
+      <div>
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
+
+export default MuiButton
